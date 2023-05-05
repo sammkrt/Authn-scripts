a4969b3e622ea81bdf2c3d75748de1b6caaed344
sammkrt <samet.krt@gmail.com>
2023-05-05 13:47:52 +0200

    Cookies Auth setup MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using Authn.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Authn.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("denied")]
+        public IActionResult Denied() 
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="admin")]
+        public IActionResult Secured()
+        {
+            return View();
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Validate(string username, string password, string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (username == "samet" && password == "123")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim("username", username));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier,username));
+                claims.Add(new Claim(ClaimTypes.Name,"Samet Kurt"));
+                
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync(claimsPrincipal);
+                return Redirect(returnUrl);
+            }
+            TempData["Error"] = "Error. Username or password is invalid";
+            return View("login");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
