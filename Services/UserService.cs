d58082fb168757262d09444ff369916a1ce9030f
sammkrt <samet.krt@gmail.com>
2023-05-07 09:29:59 +0200

    Divide name as firstName and Lastname

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -55,13 +55,27 @@ namespace Authn.Services
         internal AppUser AddNewUser(string provider, List<Claim> claims)
         {
             var appUser = new AppUser();
-            appUser.Provider = provider;    
+            appUser.Provider = provider;
             appUser.NameIdentifier = claims.GetClaim(ClaimTypes.NameIdentifier);
             appUser.Username = claims.GetClaim("username");
             appUser.Firstname = claims.GetClaim(ClaimTypes.GivenName);
-            appUser.Lastname= claims.GetClaim(ClaimTypes.Surname);
+            appUser.Lastname = claims.GetClaim(ClaimTypes.Surname);
+            var name = claims.GetClaim("name");
+            if (string.IsNullOrEmpty(appUser.Firstname))
+            {
+                appUser.Firstname = name?.Split(' ').First();
+            }
+            if (string.IsNullOrEmpty(appUser.Lastname))
+            {
+                var nameSplit = name?.Split(' ');
+                if (nameSplit.Length > 1)
+                {
+                    appUser.Lastname = name?.Split(' ').Last();
+                }
+            }
             appUser.Email = claims.GetClaim(ClaimTypes.Email);
             appUser.Mobile = claims.GetClaim(ClaimTypes.MobilePhone);
+            appUser.Roles = "NewUser";
             var entity = _context.AppUsers.Add(appUser);
             _context.SaveChanges();
             return entity.Entity;
@@ -69,11 +83,10 @@ namespace Authn.Services
 
     }
 }
-
-        public static class Extension
-        {
-            public static string GetClaim(this List<Claim> claims, string name) 
-            {
-                return claims.FirstOrDefault(c => c.Type == name)?.Value;
-            }
-        }
+public static class Extension
+{
+    public static string GetClaim(this List<Claim> claims, string name)
+    {
+        return claims.FirstOrDefault(c => c.Type == name)?.Value;
+    }
+}
